524332db646c5163f915e5566cb9b12a34646a5f
fob2 <finbarrobren95@gmail.com>
2022-06-16 12:16:50 +0000

    settings update

--- weatherApp/settings.py
+++ weatherApp/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'weather\static'),
+    os.path.join(BASE_DIR, 'weather/static'),
 )
 
 # Default primary key field type
